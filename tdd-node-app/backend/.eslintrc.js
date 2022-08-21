5eb2197b2d40a53515912322b8232811258d2389
canoe726 <canoe918@gmail.com>
2022-08-21 23:29:14 +0900

    feat: eslint packages for jest

--- tdd-node-app/backend/.eslintrc.js
+++ tdd-node-app/backend/.eslintrc.js
@@ -17,12 +17,34 @@ module.exports = {
   },
   plugins: [
     "react",
-    "@typescript-eslint"
+    "@typescript-eslint",
+    "jest"
   ],
   rules: {
     quotes: ["error", "double"],
     semi: ["error", "always"],
     indent: ["error", 2],
     "no-multi-spaces": ["error"]
-  }
+  },
+  overrides: [
+    {
+      files: [
+        "**/*.test.ts",
+        "**/*.int.test.ts"
+      ],
+      env: {
+        jest: true // now **/*.test.js files' env has both es6 *and* jest
+      },
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ]
 };
