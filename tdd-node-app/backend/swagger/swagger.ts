6f97c725045990160fdea9d343a97157128ef3a8
canoe726 <canoe918@gmail.com>
2022-08-21 23:30:44 +0900

    fix: eslint auto fix

--- tdd-node-app/backend/swagger/swagger.ts
+++ tdd-node-app/backend/swagger/swagger.ts
@@ -1,28 +1,27 @@
-import swaggerJsdoc, { Options } from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc, { Options } from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const options: Options = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      version: '1.0.0',
-      title: 'Product Test',
-      description: 'Node.js with tdd',
+      version: "1.0.0",
+      title: "Product Test",
+      description: "Node.js with tdd",
       contact: {
-        name: 'youngkim',
-        email: 'canoe918@gmail.com',
+        name: "youngkim",
+        email: "canoe918@gmail.com"
       }
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: "http://localhost:3000"
       }
     ]
   },
-  apis: ['./routes/**/*.ts', './routes/**/*.js'],
-}
+  apis: ["./routes/**/*.ts", "./routes/**/*.js"]
+};
 
-const specs = swaggerJsdoc(options)
+const specs = swaggerJsdoc(options);
 
 export { specs, swaggerUi };
-
