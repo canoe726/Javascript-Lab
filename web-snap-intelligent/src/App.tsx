ae746450d38b516ccbc33072d7d9ba49bf18e394
YoungBaeKim <canoe918@gmail.com>
2023-04-08 10:55:40 +0900

    feat: snap to grid

--- web-snap-intelligent/src/App.tsx
+++ web-snap-intelligent/src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { MouseEvent, useState } from "react";
+import "./App.css";
+
+let isMouseDown = false;
 
 function App() {
+  const [shapes, setShapes] = useState([
+    {
+      x: 0,
+      y: 0,
+      width: 200,
+      height: 150,
+      backgroundColor: "lightblue",
+      info: {
+        top: 0,
+        left: 0,
+        right: 200,
+        bottom: 0,
+      },
+    },
+    {
+      x: 250,
+      y: 200,
+      width: 150,
+      height: 150,
+      backgroundColor: "gray",
+      info: {
+        top: 0,
+        left: 0,
+        right: 200,
+        bottom: 0,
+      },
+    },
+    {
+      x: 120,
+      y: 300,
+      width: 100,
+      height: 220,
+      backgroundColor: "cornflowerblue",
+      info: {
+        top: 0,
+        left: 0,
+        right: 200,
+        bottom: 0,
+      },
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="snap-grid">
+        {shapes.map(({ width, height, x, y, backgroundColor }, index) => {
+          return (
+            <div
+              key={index}
+              className="rectangle"
+              style={{
+                width,
+                height,
+                backgroundColor,
+                transform: `translate(${x}px, ${y}px)`,
+              }}
+              onMouseDown={() => {
+                isMouseDown = true;
+              }}
+              onMouseUp={() => {
+                isMouseDown = false;
+              }}
+              onMouseMove={(event: MouseEvent<HTMLDivElement>) => {
+                if (isMouseDown) {
+                  shapes[index].x += event.movementX;
+                  shapes[index].y += event.movementY;
+
+                  setShapes([...shapes]);
+                }
+              }}
+            ></div>
+          );
+        })}
+      </div>
     </div>
   );
 }
