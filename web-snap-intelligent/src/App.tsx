993e34b3b7362e24536d080ccb256c3e43b2afc6
youngkimEBC <youngkim@ent-bc.com>
2023-05-11 12:41:42 +0900

    feat: base layout

--- web-snap-intelligent/src/App.tsx
+++ web-snap-intelligent/src/App.tsx
@@ -1,82 +1,76 @@
-import React, { MouseEvent, useState } from "react";
-import "./App.css";
+import classNames from 'classnames'
+import { MouseEvent, useRef, useState } from 'react';
+import './index.css';
 
-let isMouseDown = false;
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface ElementMeta {
+  className: string
+  position: Position
+  isSelected: boolean
+}
 
 function App() {
-  const [shapes, setShapes] = useState([
+  const canvasRef = useRef<HTMLDivElement>(null)
+
+  const [mousePosition, setMousePosition] = useState<Position>({ x: 0, y: 0 })
+
+  const elementList: ElementMeta[] = [
     {
-      x: 0,
-      y: 0,
-      width: 200,
-      height: 150,
-      backgroundColor: "lightblue",
-      info: {
-        top: 0,
-        left: 0,
-        right: 200,
-        bottom: 0,
-      },
+      className: 'w-[10rem] h-[10rem] bg-blue-300',
+      position: { x: 0, y: 0 },
+      isSelected: false,
     },
     {
-      x: 250,
-      y: 200,
-      width: 150,
-      height: 150,
-      backgroundColor: "gray",
-      info: {
-        top: 0,
-        left: 0,
-        right: 200,
-        bottom: 0,
-      },
+      className: 'w-[5rem] h-[8rem] bg-yellow-300',
+      position: { x: 500, y: 120 },
+      isSelected: false,
     },
-    {
-      x: 120,
-      y: 300,
-      width: 100,
-      height: 220,
-      backgroundColor: "cornflowerblue",
-      info: {
-        top: 0,
-        left: 0,
-        right: 200,
-        bottom: 0,
-      },
-    },
-  ]);
+  ]
+
+  const handleCanvasMouseMove = (event: MouseEvent<HTMLDivElement>) => {
+    const { x, y } = canvasRef.current?.getClientRects()[0] ?? {}
+    const { clientX, clientY } = event
+
+    const baseX = clientX - (x ?? 0);
+    const baseY = clientY - (y ?? 0);
+    setMousePosition({ x: baseX, y: baseY })
+  }
 
   return (
-    <div className="App">
-      <div id="snap-grid">
-        {shapes.map(({ width, height, x, y, backgroundColor }, index) => {
+    <div className="relative w-full h-full flex justify-center items-center p-[5rem]">
+      <div className='absolute left-[2rem] top-[2rem] w-max h-fit border-gray-600 border border-solid rounded-md px-[0.6rem] py-[0.1rem] bg-gray-50 flex text-black gap-x-[0.8rem]'>
+        <span>{`X: ${mousePosition.x}`}</span>
+
+        <span>{`Y: ${mousePosition.y}`}</span>
+      </div>
+
+      <div
+        ref={canvasRef}
+        className='relative w-full h-full overflow-hidden text-center border border-red-600 border-solid'
+        onMouseMove={handleCanvasMouseMove}
+      >
+        {elementList.map(({ className, position, isSelected }, index) => {
           return (
             <div
-              key={index}
-              className="rectangle"
+              key={`rectangle-${index}`}
+              className={
+                classNames('transition-all absolute', className, {
+                  'bg-black': isSelected,
+                })
+              }
               style={{
-                width,
-                height,
-                backgroundColor,
-                transform: `translate(${x}px, ${y}px)`,
+                transform: `translate(${position.x}px, ${position.y}px)`
               }}
-              onMouseDown={() => {
-                isMouseDown = true;
-              }}
-              onMouseUp={() => {
-                isMouseDown = false;
-              }}
-              onMouseMove={(event: MouseEvent<HTMLDivElement>) => {
-                if (isMouseDown) {
-                  shapes[index].x += event.movementX;
-                  shapes[index].y += event.movementY;
-
-                  setShapes([...shapes]);
-                }
-              }}
-            ></div>
-          );
+            />
+          )
         })}
+        
+
+        <div className='w-[5rem] h-[8rem] bg-yello-300'/>
       </div>
     </div>
   );
