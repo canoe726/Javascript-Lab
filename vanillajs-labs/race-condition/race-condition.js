93945ad0e3da3edebc1397218a04bfca00ba56bb
youngkimEBC <youngkim@ent-bc.com>
2024-02-05 13:05:39 +0900

    feat: using mutex

--- vanillajs-labs/race-condition/race-condition.js
+++ vanillajs-labs/race-condition/race-condition.js
@@ -1,4 +1,7 @@
+const { Mutex } = require('async-mutex')
+
 let balance = 0
+const mutex = new Mutex()
 
 const randomDelay = () => new Promise((resolve) => setTimeout(resolve, Math.random() * 100))
 
@@ -13,28 +16,41 @@ async function saveBalance(value) {
 }
 
 async function sellGrapes() {
-  const balance = await loadBalance()
-  console.log(`sellGrapes - balance loaded : ${balance}`)
-  const newBalance = balance + 50
-  await saveBalance(newBalance)
-  console.log(`sellGrapes - balance updated : ${newBalance}`)
+  const release = await mutex.acquire()
+  try {
+    const balance = await loadBalance()
+    console.log(`sellGrapes - balance loaded : ${balance}`)
+    const newBalance = balance + 50
+    await saveBalance(newBalance)
+    console.log(`sellGrapes - balance updated : ${newBalance}`)
+  } finally {
+    release()
+  }
 }
 
 async function sellOlives() {
-  const balance = await loadBalance()
-  console.log(`sellOlives - balance loaded : ${balance}`)
-  const newBalance = balance + 50
-  await saveBalance(newBalance)
-  console.log(`sellOlives - balance updated : ${newBalance}`)
+  const release = await mutex.acquire()
+  try {
+    const balance = await loadBalance()
+    console.log(`sellOlives - balance loaded : ${balance}`)
+    const newBalance = balance + 50
+    await saveBalance(newBalance)
+    console.log(`sellOlives - balance updated : ${newBalance}`)
+  } finally {
+    release()
+  }
 }
 
 async function main() {
-  const transaction1 = sellGrapes()
-  const transaction2 = sellOlives()
-
-  //   await transaction1
-  //   await transaction2
-  await Promise.all([transaction1, transaction2])
+  await Promise.all([
+    sellGrapes(),
+    sellOlives(),
+    sellGrapes(),
+    sellOlives(),
+    sellGrapes(),
+    sellOlives(),
+    sellGrapes(),
+  ])
 
   const balance = await loadBalance()
   console.log(`Final balance : ${balance}`)
