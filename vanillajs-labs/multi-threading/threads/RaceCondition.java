1b72cef0b0e752aadcc079d353cfe868fa630030
canoe726 <canoe918@gmail.com>
2024-02-23 18:01:04 +0900

    feat: java

--- vanillajs-labs/multi-threading/threads/RaceCondition.java
+++ vanillajs-labs/multi-threading/threads/RaceCondition.java
@@ -1,18 +1,16 @@
-class Bank implements Runnable {
-
-  private int money = 0;
-
-  public void deposit(int amount) {}
-
-  public void withdraw() {}
-
-  @Override
-  public void run() {}
-}
-
 public class RaceCondition {
 
-  public static void main(String args[]) {
-    Bank counter = new Bank();
+  public static void main(String[] args) {
+    Counter counter1 = new Counter();
+    Counter counter2 = new Counter();
+    Counter counter3 = new Counter();
+
+    Thread t1 = new Thread(counter1, "Thread-1");
+    Thread t2 = new Thread(counter2, "Thread-2");
+    Thread t3 = new Thread(counter3, "Thread-3");
+
+    t1.start();
+    t2.start();
+    t3.start();
   }
 }
