ced099bb616ec24cd6847b5934df0ab9ac86511c
canoe726 <canoe918@gmail.com>
2024-02-23 18:09:20 +0900

    fix: counter thread

--- vanillajs-labs/multi-threading/threads/RaceCondition.java
+++ vanillajs-labs/multi-threading/threads/RaceCondition.java
@@ -1,13 +1,11 @@
 public class RaceCondition {
 
   public static void main(String[] args) {
-    Counter counter1 = new Counter();
-    Counter counter2 = new Counter();
-    Counter counter3 = new Counter();
+    Counter counter = new Counter();
 
-    Thread t1 = new Thread(counter1, "Thread-1");
-    Thread t2 = new Thread(counter2, "Thread-2");
-    Thread t3 = new Thread(counter3, "Thread-3");
+    Thread t1 = new Thread(counter, "Thread-1");
+    Thread t2 = new Thread(counter, "Thread-2");
+    Thread t3 = new Thread(counter, "Thread-3");
 
     t1.start();
     t2.start();
