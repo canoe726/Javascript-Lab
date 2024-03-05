7cdc3b578c64071075ce334e9d2d359b6466f9d1
canoe726 <canoe918@gmail.com>
2024-03-05 18:28:09 +0900

    fix: ws

--- web-builder/render/app.js
+++ web-builder/render/app.js
@@ -1,26 +1,31 @@
 const http = require('http')
-const hostname = '127.0.0.1'
-const port = '4000'
+const fs = require('fs')
+const WebSocket = require('ws')
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(`
-    <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="theme-color" content="#000000" />
+const PORT = 3000
+const HOSTNAME = '127.0.0.1'
 
-            <title>render</title>
-        </head>
-        <body>
-            <div id="root">123123</div>
-        </body>
-    </html>
-    `)
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.end(getHtmlContent())
+})
+
+const wss = new WebSocket.Server({ server })
+wss.on('connection', (ws) => {
+  ws.send(getHtmlContent())
+
+  fs.watch('./server/index.js', (event, filename) => {
+    if (event === 'change') {
+      ws.send(getHtmlContent())
+    }
   })
-  .listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
-  })
+})
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is running at http://${HOSTNAME}:${PORT}`)
+})
+
+function getHtmlContent() {
+  const { renderHTML } = require('./server/index.js')
+  return renderHTML()
+}
