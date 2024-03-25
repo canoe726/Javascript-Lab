fcfa631c957f9f1c32007f96ff6bc3ee5a9b1c50
YoungBaeKim <canoe918@gmail.com>
2024-03-25 20:37:02 +0900

    feat: node

--- web-builder/render/app.js
+++ web-builder/render/app.js
@@ -9,15 +9,44 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 const PORT = 3000
 const HOSTNAME = '127.0.0.1'
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
+function watchClientFileChange() {
+  fs.watch('./client/src', 'utf-8', (event, filename) => {
+    if (event === 'change') {
+      restartServer()
+    }
+  })
+}
 
-  const indexPath = path.resolve(__dirname, './static/index.html')
-  const html = fs.readFileSync(indexPath, 'utf-8')
+let server = undefined
 
-  res.end(html.replace('__RENDER__', RenderServerDOM.render()))
-})
+function restartServer() {
+  if (server) {
+    server.close(() => {
+      console.log('Restart server...')
+      startServer()
+    })
+  } else {
+    startServer()
+  }
+}
 
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is running at http://${HOSTNAME}:${PORT}`)
-})
+function startServer() {
+  server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    const indexPath = path.resolve(__dirname, './static/index.html')
+    const html = fs.readFileSync(indexPath, 'utf-8')
+    res.end(html.replace('__RENDER__', RenderServerDOM.render()))
+  })
+
+  server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}`)
+  })
+}
+
+function main() {
+  watchClientFileChange()
+  startServer()
+}
+
+main()
