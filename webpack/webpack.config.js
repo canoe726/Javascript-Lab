00b716c360f405cbbb3633fb5c3209136f5ead8d
canoe726 <canoe918@gmail.com>
2024-04-29 13:57:53 +0900

    fix: webpack bundle without compile

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, 'dist/app.js'),
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, 'src/app.ts'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'server.bundle.js',
@@ -10,29 +12,44 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
+  stats: {
+    errorDetails: true,
+  },
+  target: 'node',
   resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    modules: ['node_modules'],
     extensions: ['.ts', '.js'],
     fallback: {
       assert: require.resolve('assert'),
       buffer: require.resolve('buffer'),
-      path: require.resolve('path-browserify'),
-      stream: require.resolve('stream-browserify'),
-      zlib: require.resolve('browserify-zlib'),
+      console: require.resolve('console-browserify'),
+      constants: require.resolve('constants-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      domain: require.resolve('domain-browser'),
+      events: require.resolve('events'),
       http: require.resolve('stream-http'),
       https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      path: require.resolve('path-browserify'),
+      punycode: require.resolve('punycode'),
+      process: require.resolve('process/browser'),
+      querystring: require.resolve('querystring-es3'),
+      stream: require.resolve('stream-browserify'),
+      string_decoder: require.resolve('string_decoder'),
+      sys: require.resolve('util'),
+      timers: require.resolve('timers-browserify'),
+      tty: require.resolve('tty-browserify'),
       url: require.resolve('url'),
       util: require.resolve('util'),
-      os: require.resolve('os-browserify/browser'),
-      querystring: require.resolve('querystring-es3'),
-      timers: require.resolve('timers-browserify'),
-      crypto: require.resolve('crypto-browserify'),
       vm: require.resolve('vm-browserify'),
+      zlib: require.resolve('browserify-zlib'),
     },
   },
 }
