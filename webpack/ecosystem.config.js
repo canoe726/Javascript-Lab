33f80f6cb32f40d3d694ca6751eac4c5a69adbc9
canoe726 <canoe918@gmail.com>
2024-04-30 17:32:23 +0900

    fix: pm2 config

--- webpack/ecosystem.config.js
+++ webpack/ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
       script: './build/main.js',
       instances: 0,
       exec_mode: 'cluster',
-      wait_ready: true,
       listen_timeout: 50000,
       kill_timeout: 5000,
     },
