0dfe991f9095113140704c651030a373ddc35509
YoungBaeKim <canoe918@gmail.com>
2024-05-22 00:01:16 +0900

    feat: nestjs initial commit

---
+++ nodejs/nestjs/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@Req() req: Request): string {
+    console.log(req);
+    return this.appService.getHello();
+  }
+}
